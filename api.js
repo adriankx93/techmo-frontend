c781af9020d3598da398936914291f8f7bf27e0b
adriankx93 <kicioradrian@gmail.com>
2025-07-03 10:12:53 +0200

    Add files via upload

---
+++ api.js
@@ -0,0 +1,26 @@
+export const API = "https://biurowiec.onrender.com"; // <-- ZMIEŃ na link backendu po deployu!
+
+export async function apiGet(path) {
+  const r = await fetch(API + path);
+  return await r.json();
+}
+export async function apiPost(path, data) {
+  const r = await fetch(API + path, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await r.json();
+}
+export async function apiPut(path, data) {
+  const r = await fetch(API + path, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await r.json();
+}
+export async function apiDelete(path) {
+  const r = await fetch(API + path, { method: "DELETE" });
+  return await r.json();
+}
