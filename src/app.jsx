a4793df134660dabcbb16451d994e014d93805f5
adriankx93 <kicioradrian@gmail.com>
2025-07-04 12:14:04 +0200

    app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
-return (
-  <div style={{padding: "3rem", fontSize: "2rem", color: "black"}}>
-    Test render: jeśli to widzisz, React działa!
-  </div>
-);
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "3rem", fontSize: "2rem", color: "black" }}>
+      Test render: jeśli to widzisz, React działa!
+    </div>
+  );
+}
